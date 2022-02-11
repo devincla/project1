d42dbd68dbfff02977b0af78388e377b5bb8e510
devincla <97700193+devincla@users.noreply.github.com>
2022-02-11 10:19:24 -0500

    2/11/2022 before presentation

--- src/main/scala/project0.scala
+++ src/main/scala/project0.scala
@@ -18,12 +18,16 @@ object project0 {
 
 
     def scenario1() : Unit = {
+      println("Selecting total number of consumers on Branch 1..")
       spark.sql("SELECT COUNT(*) FROM Branches WHERE branch = 'Branch1'").show()
+      println("Selecting total number of consumers on Branch 2...")
       spark.sql("SELECT COUNT(*) FROM Branches WHERE branch = 'Branch2'").show()
     }
     def scenario2() : Unit = {
 
+      println("Selecting the most consumed beverage on Branch 1...")
       spark.sql("SELECT Branches.bev, sum(Conscounts.count) AS sales FROM Conscounts INNER JOIN Branches ON Branches.bev = Conscounts.bev WHERE Branches.branch = 'Branch1' GROUP BY Branches.bev ORDER BY sales DESC LIMIT 1").show()
+      println("Selecting the least consumed beverage on Branch 2...")
       spark.sql("SELECT Branches.bev, sum(Conscounts.count) AS sales FROM Conscounts INNER JOIN Branches ON Branches.bev = Conscounts.bev WHERE Branches.branch = 'Branch2' GROUP BY Branches.bev ORDER BY sales ASC LIMIT 1").show()
       val df = spark.sql("SELECT Branches.bev, sum(Conscounts.count) AS sales FROM Conscounts INNER JOIN Branches ON Branches.bev = Conscounts.bev WHERE Branches.branch = 'Branch1' GROUP BY Branches.bev ORDER BY sales DESC")
       val medianDf = df.stat.approxQuantile("sales", Array(0.5), 0.25)
@@ -32,54 +36,68 @@ object project0 {
     }
 
     def scenario3() : Unit = {
-
-      spark.sql("SELECT DISTINCT bev FROM Branches WHERE branch IN ('Branch1', 'Branch8', 'Branch10') ORDER BY bev ASC").show()
+      println("Selecting all beverages available in branches 1, 8, and 10...")
+      spark.sql("SELECT DISTINCT bev FROM Branches WHERE branch IN ('Branch1', 'Branch8', 'Branch10') ORDER BY bev ASC").show(50)
+      println("Selecting all beverages common to branches 4 and 7...")
       spark.sql("SELECT DISTINCT t1.bev\nFROM Branches t1\nINNER JOIN Branches t2\non t1.bev = t2.bev\nWHERE t1.branch = 'Branch3'\nAND t2.branch = 'Branch7' ORDER BY bev ASC").show(50)
 
     }
 
     def scenario4() : Unit = {
+      println("\nDeleting table if exists...\n")
+      spark.sql("DROP TABLE IF EXISTS partitionBranches")
       println("\nCreating a table with partitions on branches 1 & 8...\n")
-      //spark.sql("CREATE TABLE IF NOT EXISTS partitionBranches (bev STRING) PARTITIONED BY (branch STRING) row format delimited fields terminated by ','")
-      //spark.sql("LOAD DATA LOCAL INPATH 'input/Bev_Branch.txt' INTO TABLE partitionBranches PARTITION(branch='Branch8')")
-      //spark.sql("LOAD DATA LOCAL INPATH 'input/Bev_Branch.txt' INTO TABLE partitionBranches PARTITION(branch='Branch1')")
+      spark.sql("CREATE TABLE IF NOT EXISTS partitionBranches (bev STRING) PARTITIONED BY (branch STRING) row format delimited fields terminated by ','")
+      spark.sql("LOAD DATA LOCAL INPATH 'input/Bev_Branch.txt' INTO TABLE partitionBranches PARTITION(branch='Branch8')")
+      spark.sql("LOAD DATA LOCAL INPATH 'input/Bev_Branch.txt' INTO TABLE partitionBranches PARTITION(branch='Branch1')")
       spark.sql("DESCRIBE FORMATTED partitionBranches").show()
       spark.sql("SELECT * FROM partitionBranches").show()
     }
 
     def scenario5() : Unit = {
-      //spark.sql("DROP TABLE noteTable")
+      println("\nDropping table if exists...\n")
+      spark.sql("DROP TABLE IF EXISTS noteTable")
       println("\nCreating table...\n")
       spark.sql("CREATE TABLE IF NOT EXISTS noteTable (bev STRING, branch STRING) row format delimited fields terminated by ','")
-      //println("\nLoading data into table...\n")
-      //spark.sql("LOAD DATA LOCAL INPATH 'input/Bev_Branch.txt' INTO TABLE noteTable")
+      println("\nLoading data into table...\n")
+      spark.sql("LOAD DATA LOCAL INPATH 'input/Bev_Branch.txt' INTO TABLE noteTable")
       println("\nAdding table note...\n")
       spark.sql("ALTER TABLE noteTable SET TBLPROPERTIES('notes' = 'This is a note')")
       spark.sql("SHOW TBLPROPERTIES noteTable").show()
-//      println("\nDeleting row from table...\n")
-//      spark.sql("ALTER TABLE partitionbranches DROP IF EXISTS PARTITION(branch='Branch1')")
+      println("\nDeleting row from table...\n")
+      spark.sql("ALTER TABLE partitionBranches DROP IF EXISTS PARTITION(branch='Branch1')")
+      spark.sql("SELECT * FROM partitionBranches").show()
 
 
     }
 
     def scenario6() : Unit = {
       println("This is my future query")
-      //spark.sql("DROP TABLE bevSales")
-//      println("\nCreating table...\n")
-      spark.sql("CREATE TABLE IF NOT EXISTS bevSales (bev STRING, sales BIGINT, modifier STRING) ROW FORMAT DELIMITED FIELDS TERMINATED BY ','")
-//      println("\nLoading data into table...\n")
-      //spark.sql("LOAD DATA LOCAL INPATH 'input/bevSalestxt.txt' INTO TABLE bevSales")
+      spark.sql("DROP TABLE IF EXISTS bevSales")
+      println("\nCreating table...\n")
+      spark.sql("CREATE TABLE IF NOT EXISTS bevSales (bev STRING, sales BIGINT, modifier STRING, bevType STRING) ROW FORMAT DELIMITED FIELDS TERMINATED BY ','")
+      println("\nLoading data into table...\n")
+      spark.sql("LOAD DATA LOCAL INPATH 'input/bevSalestxt.txt' INTO TABLE bevSales")
       println("\nOrganizing beverages by modifier popularity..\n")
-      spark.sql("select sum(sales) as totalSales from bevSales where bev like '%cold%' union " +
-        "select sum(sales) as totalSales from bevSales where bev like '%double%' union " +
-        "select sum(sales) as totalSales from bevSales where bev like '%icy%' union " +
-        "select sum(sales) as totalSales from bevSales where bev like '%large%' union " +
-        "select sum(sales) as totalSales from bevSales where bev like '%med%' union " +
-        "select sum(sales) as totalSales from bevSales where bev like '%mild%' union " +
-        "select sum(sales) as totalSales from bevSales where bev like '%small%' union " +
-        "select sum(sales) as totalSales from bevSales where bev like '%triple%' union " +
-        "select sum(sales) as totalSales from bevSales where bev like '%special%' order by totalSales desc").show()
+      spark.sql("SELECT * FROM bevSales").show()
+      println("\nOrganizing beverages by most to least popular beverage types...\n")
+      spark.sql("select bevType, sum(sales) as totalSales from bevSales where bevType = 'cappuccino' GROUP BY bevType union " +
+        "select bevType, sum(sales) as totalSales from bevSales where bevType = 'coffee' GROUP BY bevType union " +
+        "select bevType, sum(sales) as totalSales from bevSales where bevType = 'espresso' GROUP BY bevType union " +
+        "select bevType, sum(sales) as totalSales from bevSales where bevType = 'latte' GROUP BY bevType union " +
+        "select bevType, sum(sales) as totalSales from bevSales where bevType = 'lite' GROUP BY bevType union " +
+        "select bevType, sum(sales) as totalSales from bevSales where bevType = 'mocha' GROUP BY bevType union order by totalSales desc").show()
 
+      println("\nOrganzing beverages by most to least popular modifier...\n")
+      spark.sql("select modifier, sum(sales) as totalSales from bevSales where modifier = 'cold' GROUP BY modifier union " +
+        "select modifier, sum(sales) as totalSales from bevSales where modifier = 'double' GROUP BY modifier union " +
+        "select modifier, sum(sales) as totalSales from bevSales where modifier = 'icy' GROUP BY modifier union " +
+        "select modifier, sum(sales) as totalSales from bevSales where modifier = 'large' GROUP BY modifier union " +
+        "select modifier, sum(sales) as totalSales from bevSales where modifier = 'med' GROUP BY modifier union " +
+        "select modifier, sum(sales) as totalSales from bevSales where modifier = 'mild' GROUP BY modifier union " +
+        "select modifier, sum(sales) as totalSales from bevSales where modifier = 'small' GROUP BY modifier union " +
+        "select modifier, sum(sales) as totalSales from bevSales where modifier = 'triple' GROUP BY modifier union " +
+        "select modifier, sum(sales) as totalSales from bevSales where modifier = 'special' GROUP BY modifier order by totalSales desc").show()
     }
 
     def scenarioDrop() : Unit = {
@@ -98,7 +116,8 @@ object project0 {
     var quit = false
     while(quit == false){
 
-      println("\n\nChoose scenario: \n1 (Select)\n2 (Most, least, average)\n3 (Common Bevs) \n4 (Partition) \n5 (Note and Remove) \n6 (Future Query) \n7 (Drop Tables)\n8 (Quit Program)")
+      println("\n\nChoose scenario: \n1) (Total consumers)\n2) (Most, least, average)\n3) (Common Bevs) \n4) (Partition) \n" +
+        "5) (Note and Remove) \n6) (Future Query) \n7) (Drop Tables)\n8) (Quit Program)")
       val i = scanner.nextInt()
       i match {
         case 1 => scenario1()
